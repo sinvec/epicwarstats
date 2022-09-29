96153d40ad7d9505493b5775fe5ebc2746ebd553
U-WIN-3F9R2A34ICF\wind <web.vadim@list.ru>
2022-09-29 16:50:15 +0500

    Db name was changed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from scraper import EpicWarScraper
 from store import EpicWarStore
 import psycopg2
 
-db_connection = psycopg2.connect(dbname="pet2", user="postgres", password="123456789")
+db_connection = psycopg2.connect(dbname="epicwartest", user="postgres", password="123456789")
 my_store = EpicWarStore(db_connection)
 my_scraper = EpicWarScraper(my_store)
 my_scraper.start()
